d79f666a5ca078b88556bd34e7cea7991c14d2dc
2ghszhaoh819 <75029685+2ghszhaoh819@users.noreply.github.com>
2021-03-10 00:27:43 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -29,8 +29,8 @@ let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
 const randomCount = $.isNode() ? 20 : 5;
 const inviteCodes = [
-  `YFjh6Vll-l3zb9cCf_U@aURoM7PtY_Q@eU9YL5XqGLxSmRSAkwxR@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA@eU9YMZ_gPpRurC-foglg`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA`
+  `eU9Yar3jZv8h-G7Rz3NH1Q@eU90JrTjOaQ@eU9Yabq0Z65y-TvVw3oW3g@eU9YMYz7Jrlyjh2NjiJ5@eU9YOZTEI4NulQuEiyFg@eU9YaL3kZvok9TvSzXIV0A`,
+  `eU9Yar3jZv8h-G7Rz3NH1Q@eU90JrTjOaQ@eU9Yabq0Z65y-TvVw3oW3g@eU9YMYz7Jrlyjh2NjiJ5@eU9YOZTEI4NulQuEiyFg@eU9YaL3kZvok9TvSzXIV0A`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
